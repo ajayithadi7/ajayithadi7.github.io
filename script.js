bae4c52eae32ee01dbc929dd5b1c17d2fcd9a55f
Ajay Ithadi <ajayithadi7@gmail.com>
2026-03-08 22:48:07 +0530

    Initialize AOS and particles.js in script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+AOS.init();
+
+particlesJS("particles-js", {
+
+particles: {
+
+number: { value: 60 },
+
+color: { value: "#6366f1" },
+
+shape: { type: "circle" },
+
+opacity: { value: 0.5 },
+
+size: { value: 3 },
+
+move: { enable: true, speed: 2 }
+
+}
+
+});
